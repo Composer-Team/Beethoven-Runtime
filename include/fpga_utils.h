4ccb5da0fe2596668467d42d96ce09f47c9d6d21
Chris Kjellqvist <chriskjellqvist@gmail.com>
2022-11-13 14:12:40 -0500

    extern C

--- include/fpga_utils.h
+++ include/fpga_utils.h
@@ -8,13 +8,14 @@
 #include "fpga_mgmt.h"
 #include "fpga_pci.h"
 
+extern "C" {
+
 extern pthread_mutex_t bus_lock;
 extern int slot_id;
 extern int pci_bar_handle;
 extern int xdma_write_fd;
 extern int xdma_read_fd;
 extern pthread_mutex_t main_lock;
-
 
 
 void check_rc(int rc, const char *message);
@@ -28,6 +29,7 @@ int wrapper_fpga_dma_burst_write(int fd, uint8_t *buffer, size_t xfer_sz,
 
 int wrapper_fpga_dma_burst_read(int fd, uint8_t *buffer, size_t xfer_sz,
                                 size_t address);
+}
 
 
 #endif //COMPOSER_VERILATOR_FPGA_UTILS_H
