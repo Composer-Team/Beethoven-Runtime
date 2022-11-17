4d0a937ef254fe0223f9d0c95eb843fcc203d6a8
Chris Kjellqvist <chriskjellqvist@gmail.com>
2022-11-17 09:09:01 -0500

    sanity

---
+++ src/xsim_sanity.cc
@@ -0,0 +1,24 @@
+//
+// Created by Chris Kjellqvist on 11/17/22.
+//
+#include "composer_allocator_declaration.h"
+#include "fpga_pci_sv.h"
+#include "sh_dpi_tasks.h"
+#include <composer/rocc_cmd.h>
+
+extern "C" void test_main(int *rc) {
+  auto co = composer::rocc_cmd::start_cmd(ALUSystem_ID, 0, 0, true, composer::R0, 0, 0, 0, 32,  64);
+  auto p = co.pack(pack_cfg);
+  pci_bar_handle_t pci_bar_handle = PCI_BAR_HANDLE_INIT;
+  for (int i = 0; i < 5; ++i) {
+    cosim_printf("testing %d\n", i);
+    uint32_t ready = 0;
+    while (!ready) {
+      fpga_pci_peek(pci_bar_handle, CMD_READY, &ready);
+      cosim_printf("READY: %d\n", ready);
+    }
+    fpga_pci_poke(pci_bar_handle, CMD_BITS, p[i]);
+    fpga_pci_poke(pci_bar_handle, CMD_VALID, 1);
+  }
+  *rc = 0;
+}
