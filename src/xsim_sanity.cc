b908fd2bbead698b15057c3ef699c0b2c0912ac5
Chris Kjellqvist <chriskjellqvist@gmail.com>
2022-11-17 09:40:39 -0500

    sanity

--- src/xsim_sanity.cc
+++ src/xsim_sanity.cc
@@ -7,8 +7,14 @@
 #include <composer/rocc_cmd.h>
 
 extern "C" void test_main(int *rc) {
+  cosim_printf("enter function\n");
+  fpga_pci_init();
+  fpga_mgmt_init();
+  cosim_printf("initialized\n");
+
   auto co = composer::rocc_cmd::start_cmd(ALUSystem_ID, 0, 0, true, composer::R0, 0, 0, 0, 32,  64);
   auto p = co.pack(pack_cfg);
+  cosim_printf("packed command\n");
   pci_bar_handle_t pci_bar_handle = PCI_BAR_HANDLE_INIT;
   for (int i = 0; i < 5; ++i) {
     cosim_printf("testing %d\n", i);
@@ -20,5 +26,7 @@ extern "C" void test_main(int *rc) {
     fpga_pci_poke(pci_bar_handle, CMD_BITS, p[i]);
     fpga_pci_poke(pci_bar_handle, CMD_VALID, 1);
   }
+
+  fpga_mgmt_close();
   *rc = 0;
 }
