67fd9684a3a065612a2dc13ebfdfa9f48bde4058
Chris Kjellqvist <chriskjellqvist@gmail.com>
2022-11-17 13:27:00 -0500

    sanity

--- src/xsim_sanity.cc
+++ src/xsim_sanity.cc
@@ -2,13 +2,20 @@
 // Created by Chris Kjellqvist on 11/17/22.
 //
 #include "composer_allocator_declaration.h"
+
+#include <composer/rocc_cmd.h>
+
+#ifndef USEMAIN
 extern "C" {
 #include "fpga_pci_sv.h"
 #include "sh_dpi_tasks.h"
 };
-#include <composer/rocc_cmd.h>
-
 extern "C" void test_main_hook(int *rc) {
+#else
+#define cosim_printf printf
+#include <fpga_utils.h>
+int main() {
+#endif
   cosim_printf("enter function\n");
   fflush(stdout);
   fpga_mgmt_init();
@@ -17,7 +24,13 @@ extern "C" void test_main_hook(int *rc) {
 
   uint32_t p[] = {0x2200407b, 0x0, 0x20, 0x0, 0x40};
 
+#ifndef USEMAIN
   pci_bar_handle_t pci_bar_handle = PCI_BAR_HANDLE_INIT;
+#else
+  pci_bar_handle = PCI_BAR_HANDLE_INIT;
+  int rc = fpga_pci_attach(0, FPGA_APP_PF, APP_PF_BAR0, 0, &pci_bar_handle);
+  check_rc(rc, "fpga_pci_attach FAILED");
+#endif
   for (int i = 0; i < 5; ++i) {
     cosim_printf("testing %d\n", i);
     uint32_t ready = 0;
@@ -38,7 +51,8 @@ extern "C" void test_main_hook(int *rc) {
     fpga_pci_poke(pci_bar_handle, RESP_READY, 1);
   }
 
-
   fpga_mgmt_close();
+#ifndef USEMAIN
   *rc = 0;
+#endif
 }
