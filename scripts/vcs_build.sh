5e89c1531e02330ac1ab9700b205cad0901846b1
Chris Kjellqvist <chriskjellqvist@gmail.com>
2024-08-08 10:10:08 -0400

    vcs

---
+++ scripts/vcs_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make install
+vcs +v2k -kdb -debug_access -P $VERDI_HOME/share/PLI/VCS/LINUX64/novas.tab $VERDI_HOME/share/PLI/VCS/LINUX64/pli.a -sverilog +incdir+$BEETHOVEN_ROOT/Beethoven-Hardware/vsim/generated-src/beethoven.build -full64 +vpi+1 -CFLAGS -std=c++17 -P tab.tab  +define+CLOCK_PERIOD=1 -f $BEETHOVEN_ROOT/Beethoven-Hardware/vsim/generated-src/vcs_srcs.in libBeethovenRuntime.a $BEETHOVEN_ROOT/lib/libdramsim3.so -lrt -L/usr/local/lib64 -lbeethoven -CFLAGS -I$BEETHOVEN_ROOT/Beethoven-Hardware/vsim/generated-src/ ../../Beethoven/Beethoven-Hardware/vsim/generated-src/beethoven.build/BeethovenTopVCSHarness.v -o BeethovenTop
