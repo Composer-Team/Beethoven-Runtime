89e3e999ac01ebd9e8206195cb7a5874fe4141f2
Chris Kjellqvist <chriskjellqvist@gmail.com>
2024-11-14 18:05:37 -0500

    switch for new paths

--- tests/memcpy.cc
+++ tests/memcpy.cc
@@ -4,8 +4,19 @@
 
 #include "fpga_utils.h"
 #include <mmio.h>
-
+#include <thread>
+#include <iostream>
+using namespace std::chrono_literals;
 int main() {
   fpga_setup(0);
-  mmio_poke();
+  auto ntxs = 64;
+  poke_mmio(0, 64);
+  std::this_thread::sleep_for(5s);
+  uint64_t q = peek_mmio(0);
+  auto n_cycles = q * 32;
+  auto n_bytes = ntxs * 64 * 64;
+  double s(n_cycles);
+  s /= 250 * 1000 * 1000;
+  double GB = double(n_bytes) / 1024 / 1024 / 1024;
+  std::cout << n_bytes << " in " << n_cycles << "cycles: " << (GB / s) << "GB/s" << std::endl;
 }
