e543491a71b4d630b5eb93d9308ef0c9f4bb5a22
Chris Kjellqvist <chriskjellqvist@gmail.com>
2025-03-12 08:35:50 -0400

    faster

--- scripts/vcs_build.sh
+++ scripts/vcs_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make install
+make -j install
 # vcs +v2k -kdb -debug_access -P $VERDI_HOME/share/PLI/VCS/LINUX64/novas.tab $VERDI_HOME/share/PLI/VCS/LINUX64/pli.a -sverilog +incdir+$SDIR -full64 +vpi+1 -CFLAGS -std=c++17 -P tab.tab  +define+CLOCK_PERIOD=1 -f $SLIST libBeethovenRuntime.a $BEETHOVEN_ROOT/lib/libdramsim3.so -lrt -L/usr/local/lib64 -lbeethoven -CFLAGS -I$BEETHOVEN_ROOT/Beethoven-Hardware/vsim/generated-src/ $BEETHOVEN_ROOT/Beethoven-Hardware/vsim/generated-src/beethoven.build/BeethovenTopVCSHarness.v -o BeethovenTop
 # if SDIR is not set, set it to $BEETHOVEN_ROOT/Beethoven-Hardware/vsim/generated-src/beethoven.build
 SDIR=${SDIR:-$BEETHOVEN_PATH/build/hw/}
